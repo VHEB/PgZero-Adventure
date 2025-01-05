4bb749601d0cc3d600583e22df46d5721a13eac4
VHEB <vheb0316@gmail.com>
2025-01-05 13:56:08 -0300

    Adicionado colisão e contagem de moedas, E atualização de formato dos sons

---
+++ Class/collectable.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame import image
+
+class Collectable:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 16
+        self.height = 16
+        self.sprite_sheet = image.load("images/coin.png")
+        self.num_frames = 12
+        self.frames = []
+        self.collected = False
+
+        for i in range(self.num_frames):
+            frame_rect = pygame.Rect(i * self.width, 0, self.width, self.height)
+            frame_image = self.sprite_sheet.subsurface(frame_rect)
+            self.frames.append(frame_image)
+        
+        self.current_frame = 0
+        self.last_frame_time = pygame.time.get_ticks()
+        self.frame_interval = 100
+    
+    def get_sprite(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_frame_time >= self.frame_interval:
+            self.current_frame = (self.current_frame + 1) % self.num_frames
+            self.last_frame_time = current_time
+        
+        return self.frames[self.current_frame]
+    
+    @property
+    def rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+    
+    def collect(self):
+        self.collected = True
+    
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pgzrun
 from pygame import image, transform
 from Class.hero import Hero
+from Class.collectable import Collectable
 
 WIDTH = 800
 HEIGHT = 600
 
 hero = Hero(100, 300)
+collerctables = [
+    Collectable(300, 525),
+    Collectable(500, 525),
+    Collectable(700, 525)
+]
+
+coins_collected = 0
+font_size = 30
 
 buttons = [
     {"text": "Start", "x": 300, "y": 200, "width": 200, "height": 70, "action": "start_game"},
@@ -22,8 +31,10 @@ platform_width = platform_image.get_width()
 platform_height = platform_image.get_height()
 platform_rect = Rect(0, HEIGHT - platform_height, WIDTH, platform_height) 
 
-music.set_volume(0.5)  
-music.play("background.mp3")  
+
+sounds.background.play()
+sounds.background.set_volume(0.5)
+
 
 def draw():
     screen.clear()
@@ -34,6 +45,12 @@ def draw():
         hero.update(platform_rect)  # Atualiza o herói com movimentação
         screen.blit(hero.get_sprite(), (hero.x, hero.y))
         
+        for collectable in collerctables:
+            if not collectable.collected:
+                screen.blit(collectable.get_sprite(), (collectable.x, collectable.y))
+
+        screen.draw.text(f"Coins:  {coins_collected}", (10, 10), fontsize=font_size, color="black")
+    
     if current_screen == "menu":
         draw_menu()
 
@@ -44,14 +61,9 @@ def draw_menu():
         screen.draw.filled_rect(Rect((button["x"], button["y"]), (button["width"], button["height"])), "gray")
         screen.draw.text(button["text"], center=(button["x"] + button["width"] // 2, button["y"] + button["height"] // 2), fontsize=30, color="white")
 
-def on_mouse_down(pos, button):
+def on_mouse_down(pos):
     global current_screen
     
-    if current_screen == "game":
-        if button == mouse.LEFT and hero.rect.collidepoint(pos):
-            print("Clicou na tela")
-
-
     if current_screen == "menu":
         for button in buttons:
             button_rect = Rect((button["x"], button["y"]), (button["width"], button["height"]))
@@ -76,6 +88,15 @@ def on_key_up(key):
         if key in [keys.RIGHT, keys.LEFT]:
             hero.velocity_x = 0
 def update():
+    global coins_collected
     if current_screen == "game":
         hero.update(platform_rect)
+
+        for collectable in collerctables:
+            if not collectable.collected and hero.rect.colliderect(collectable.rect):
+                collectable.collected = True
+                coins_collected += 1
+                sounds.coin.play()
+                sounds.coin.set_volume(0.5)
+
 pgzrun.go()
